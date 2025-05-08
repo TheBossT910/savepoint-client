7aa71152e7b6b9f1253b5e174b9c8339d2afc173
Taha Rashid <taharashid710192@gmail.com>
2025-05-08 11:15:39 -0400

    chore: align view in center

--- app/(tabs)/details.tsx
+++ app/(tabs)/details.tsx
@@ -3,15 +3,30 @@
 // Product details view
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 // main screen
 const DetailsScreen = () => {
+    // getting styles
+    const { styles } = useStyles();
+
     return(
-        <View>
+        <View style={ styles.center }>
             <Text>Hello, world!</Text>
         </View>
-    )
+    );
+};
+
+const useStyles = () => {
+    const styles = StyleSheet.create({
+        center: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+    });
+
+    return { styles };
 };
 
 export default DetailsScreen;
