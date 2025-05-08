99984734b8a80bf7d81b125425e6fc8ed7ad4bd5
Taha Rashid <taharashid710192@gmail.com>
2025-05-08 11:11:37 -0400

    chore: created details.tsx view
    This is the product details view

---
+++ app/(tabs)/details.tsx
@@ -0,0 +1,17 @@
+// Taha Rashid
+// May 8, 2025
+// Product details view
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// main screen
+const DetailsScreen = () => {
+    return(
+        <View>
+            <Text>Hello, world!</Text>
+        </View>
+    )
+};
+
+export default DetailsScreen;
