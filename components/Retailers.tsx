399671e0f3ef5d7e44cbcacbf3a6dcd9b5363cb0
Taha Rashid <taharashid710192@gmail.com>
2025-05-10 16:44:36 -0400

    chore: created Retailers.tsx
    This is the props file to display retailers

---
+++ components/Retailers.tsx
@@ -0,0 +1,18 @@
+// Taha Rashid
+// May 10, 2025
+// view to display product retailers
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+type RetailerProps = {
+    name: string;
+}
+
+export const Retailer = ( props: RetailerProps ) => {
+    return(
+        <View>
+            <Text>Hello, { props.name }!</Text>
+        </View>
+    );
+}
