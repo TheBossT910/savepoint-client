71e32de9b68fc668c4e8d48c5e5c66aa4dce678f
Taha Rashid <taharashid710192@gmail.com>
2025-05-10 16:17:07 -0400

    chore: created layout.tsx and layoutExample.tsx files

---
+++ app/(tabs)/examples/layout.tsx
@@ -0,0 +1,6 @@
+// Taha Rashid
+// May 10, 2025
+// Layout example screen
+
+import LayoutScreen from "@/screens/examples/layoutExample";
+export default LayoutScreen;
---
+++ screens/examples/layoutExample.tsx
@@ -0,0 +1,16 @@
+// Taha Rashid
+// May 10, 2025
+// an example view to show how stacking elements works
+
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const LayoutScreen = () => {
+    return (
+        <View>
+            <Text>Hello, world</Text>
+        </View>
+    );
+};
+
+export default LayoutScreen;
