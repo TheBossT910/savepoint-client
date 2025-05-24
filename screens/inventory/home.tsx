5243081eb16b9b5e685f0cb5f51a81939d17847f
Taha Rashid <taharashid710192@gmail.com>
2025-05-24 11:10:21 -0400

    chore: moved product inventory item to its own component

--- screens/inventory/home.tsx
+++ screens/inventory/home.tsx
@@ -3,57 +3,39 @@
 // Inventory home screen
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import useStyles from './styles';
+import ProductCard from '@/components/ProductCard';
+import { IProductsInventory } from '@/interfaces/api';
 
 const InventoryHomeScreen = () => {
     const { styles } = useStyles();
+
+    // testing object
+    const product: IProductsInventory = {
+        type: {
+            new: 2,
+            complete: 5,
+            used: 23
+        },
+        condition: {
+            new: 2,
+            excellent: 4,
+            veryGood: 10,
+            good: 7,
+            acceptable: 9
+        },
+        info: {
+            name: 'Super Mario World',
+            cover: 'https://images.igdb.com/igdb/image/upload/t_1080p/co8lo8.jpg'
+        }
+    }
+
     return(
-        <View style={ styles.center }>
-            <Text>Hello, world!</Text>
-            {/* Product card */}
-            <View style={ styles.productCard }>
-                {/* Cover section */}
-                <View style={ styles.coverSection }>
-                    {/* Total # */}
-                    <View style={ styles.totalProductsArea }>
-                        <Text>Total #</Text>
-                    </View>
-
-                    {/* Add button */}
-                    <View style={ styles.addButtonArea }>
-                        <Text>Add btn</Text>
-                    </View>
-                </View>
-
-                {/* Title section */}
-                <View style={ styles.titleSection }>
-                    {/* Title */}
-                    <View style={ styles.titleArea }>
-                        <Text>Title of Product</Text>
-                    </View>
-
-                    {/* Conditions */}
-                    <View style={ styles.conditionsArea }>
-                        <View style={ styles.column }>
-                            {/* Conditon type */}
-                            <View style={ styles.row }>
-                                <Text>New </Text>
-                                <Text>Complete </Text>
-                                <Text>Used</Text>
-                            </View>
-
-                            {/* Condition (specific) */}
-                            <View style={ styles.row }>
-                                <Text>Excellent </Text>
-                                <Text>Very Good </Text>
-                                <Text>Good </Text>
-                                <Text>Acceptable</Text>
-                            </View>
-                        </View>
-                    </View>
-                </View>
-            </View>
+        <View style={ styles.container }>
+            <ProductCard product={ product }></ProductCard>
+            <ProductCard product={ product }></ProductCard>
+            <ProductCard product={ product }></ProductCard>
         </View>
     )
 }
